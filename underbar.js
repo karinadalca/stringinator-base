12d6e295249e3918d03e148e22b8a93b8da83c80
Karina Dalca <karinadalca@gmail.com>
2017-05-08 00:22:03 -0700

    retried rewriting underbar functions

--- underbar.js
+++ underbar.js
@@ -5,7 +5,7 @@ const identity = function(val) {
 
 // Returns the first n elements of the given array.
 const first = function(array, n = 1) {
-  return n === 1 ? array[0] : array.slice(0, n);
+  return n === 1 ? array[0] : array.splice(0, n);
 };
 
 // Returns the last n elements of the given array.
@@ -15,26 +15,26 @@ const last = function(array, n = 1) {
 
 // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 const indexOf = function(array, target, fromIndex=0) {
- for (let index = fromIndex; index <= array.length; index++) {
-    if (array[index] === target) {
-      return index;
+  for (let i = fromIndex; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
     }
   }
-  return -1;
+  return -1
 };
 
 const isArrayLike = function(obj) {
   const length = obj['length'];
-  return typeof length === 'number' && length >= 0;
+  return typeof length && length >= 0 ? true : false;
 };
 
 // The cornerstone of a functional library -- iterate all elements, pass each to a callback function.
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 const each = function(obj, callback=identity) {
-  if (isArrayLike(obj)) {
-    for (let index = 0; index < obj.length; index++) {
-      callback(obj[index], index, obj);
-    }
+  if (isArrayLike(obj)){
+    for (let i = 0; i < obj.length; i++) {
+      callback(obj[i], i, obj);
+    } 
   } else {
     for (let key in obj) {
       callback(obj[key], key, obj);
@@ -45,17 +45,17 @@ const each = function(obj, callback=identity) {
 // Return the results of applying the callback to each element.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 const map = function(obj, callback=identity) {
-  const results = [];
-  each(obj, (currentValue, currentIndexOrKey, obj) => {
-    results.push(callback(currentValue, currentIndexOrKey, obj));
-  });
+  let results = [];
+  each(obj, (value, index, obj) => {
+    results.push(callback(value, index, obj))
+    });
   return results;
 };
 
 // Return an array of the values of a certain property in the collection.
 // E.g. given an array of people objects, return an array of just their ages.
 const pluck = function(obj, key) {
-  return map(obj, item => item[key]);
+  return map(obj, element => element[key])
 };
 
 // Reduces collection to a value which is the accumulated result of running
@@ -87,24 +87,36 @@ const contains = function(obj, target) {
 
 // Return true if all the elements / object values are accepted by the callback.
 const every = function(obj, callback=identity) {
-  return reduce(obj, (allPassed, item) => {
-    return allPassed && !!callback(item);
-  }, true);
+  var count = 0;
+  for (var i = 0; i < obj.length; i++){
+    if (callback(obj[i], i, obj)) {
+      count++;
+    }
+  }
+  if (count === obj.length){
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Return true if even 1 element / object value is accepted by the callback.
 const some = function(obj, callback=identity) {
-  return reduce(obj, (anyPassed, item) => {
-    return anyPassed || !!callback(item);
-  }, false);
+  var count = 0;
+  for (var i = 0; i < obj.length; i++){
+    if (callback(obj[i], i, obj)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Return an array with all elements / object values that are accepted by the callback.
 const filter = function(obj, callback=identity) {
   const result = [];
-  each(obj, item => {
-    if (callback(item)) {
-      result.push(item);
+  each(obj, element => {
+    if (callback(element)) {
+      result.push(element);
     }
   });
   return result;
@@ -112,7 +124,7 @@ const filter = function(obj, callback=identity) {
 
 // Return object without the elements / object valuesthat were rejected by the callback.
 const reject = function(arr, callback=identity) {
-  return filter(arr, item => !callback(item));
+  return filter(arr, element => !callback(element));
 };
 
 // De-duplicates (de-dups) the elements / object values.
